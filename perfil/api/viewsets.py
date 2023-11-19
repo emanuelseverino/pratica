f33ccdacc2f52b4693a044dac3b4b4213c0955ca
Emanuel Severino <emanuelsmseverino@gmail.com>
2023-11-19 12:42:24 -0300

    Render Class

--- perfil/api/viewsets.py
+++ perfil/api/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.renderers import JSONRenderer
 from rest_framework.viewsets import ModelViewSet
 
 from perfil.api.serializers import PerfilSerializer
@@ -12,6 +13,7 @@ class PerfilViewSet(ModelViewSet):
     serializer_class = PerfilSerializer
     permission_classes = [IsAuthenticated, ExpirationPermission]
     authentication_classes = [TokenAuthentication, ]
+    renderer_classes = [JSONRenderer,]
 
     def get_object(self):
         return User.objects.get(email=self.request.user.email)
