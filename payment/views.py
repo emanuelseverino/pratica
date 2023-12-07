2f39892b39d61d48240e015e5ab33d9d697cf6fc
Emanuel Severino <emanuelsmseverino@gmail.com>
2023-12-07 14:53:26 -0300

    Hook

--- payment/views.py
+++ payment/views.py
@@ -142,7 +142,7 @@ def readHook(url):
 class WebHook(View):
 
     def post(self, request, *args, **kwargs):
-        Texto.objects.create(texto=str(self.request))
+        Texto.objects.create(texto=str(self.request.body))
         pass
         # body = json.loads(self.request.body)
         # if body['resource']:
