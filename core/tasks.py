8166ac07e8c53b3b6cea8cb12dbfec8188a995bb
Emanuel Severino <emanuelsmseverino@gmail.com>
2023-12-15 16:04:03 -0300

    Demon

--- core/tasks.py
+++ core/tasks.py
@@ -1,7 +1,3 @@
-# Create your tasks here
-
-from demoapp.models import Widget
-
 from celery import shared_task
 
 
@@ -24,9 +20,3 @@ def xsum(numbers):
 def count_widgets():
     return Widget.objects.count()
 
-
-@shared_task
-def rename_widget(widget_id, name):
-    w = Widget.objects.get(id=widget_id)
-    w.name = name
-    w.save()
